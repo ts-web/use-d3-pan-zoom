91c9d197dca6cc1aabce6ec45171ab34eb180d65
Matthias Dailey <matthias.dailey@gmail.com>
2024-03-19 01:45:46 -0400

    d3 Zoomable Scatterplot

---
+++ src/stories/d3-zoomable-scatterplot.stories.tsx
@@ -0,0 +1,226 @@
+import { scaleLinear, scaleOrdinal } from 'd3-scale';
+import { schemeCategory10 } from 'd3-scale-chromatic';
+import { randomNormal } from 'd3-random';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { useRev } from 'use-rev';
+import { Axis } from 'react-d3-axis-ts';
+
+import { normalizeWheelDelta } from '~/panZoom-utils';
+import { usePanZoom } from '~/usePanZoom';
+
+
+export default {
+  title: 'D3 Examples',
+};
+
+const chartWidth = 800;
+const chartHeight = 600;
+const k = chartHeight / chartWidth;
+
+const data = (() => {
+  const random = randomNormal(0, 0.2);
+  const sqrt3 = Math.sqrt(3);
+  return ([] as [number, number, number][]).concat(
+    Array.from({length: 300}, () => [random() + sqrt3, random() + 1, 0]),
+    Array.from({length: 300}, () => [random() - sqrt3, random() + 1, 1]),
+    Array.from({length: 300}, () => [random(), random() - 1, 2])
+  );
+})();
+const colorScale = scaleOrdinal<string>()
+  .domain(data.map(d => d[2]) as unknown as string[])
+  .range(schemeCategory10)
+;
+
+export function ZoomableScatterplot () {
+  const [chartElement, setChartElement] = useState<Element | null>();
+
+  // When the chartElement is resolved, prevent the default action of certain events:
+  //   - touchstart — or else touch events on the chart will sometimes get intercepted by the browser for scrolling, page navigation ("swipe"), or full-page pixelated zooming.
+  //   - wheel — so that zooming the chart doesn't cause page scrolling.
+  //
+  // Note: this can't be done inline because JSX syntax doesn't support passing `{passive: false}` when registering event listener callbacks.
+  // See https://github.com/facebook/react/issues/6436
+  useEffect(() => {
+    if (!chartElement) return;
+    const preventDefault = (e: Event) => {e.preventDefault();};
+    chartElement.addEventListener('touchstart', preventDefault, {passive: false});
+    chartElement.addEventListener('wheel', preventDefault, {passive: false});
+    return () => {
+      chartElement.removeEventListener('touchstart', preventDefault);
+      chartElement.removeEventListener('wheel', preventDefault);
+    }
+  }, [chartElement]);
+
+  // Track the chart's offset, to be used when we calculate a pointer's position relative to the chart.
+  const chartOffset = useRef({x: 0, y: 0});
+  const updateChartOffset = () => {
+    if (!chartElement) return;
+    const rect = chartElement.getBoundingClientRect();
+    chartOffset.current = {
+      x: rect.x,
+      y: rect.y,
+    };
+  }
+
+  const xScale = useMemo(() => {
+    const _xScale = scaleLinear();
+    _xScale.domain([-4.5, 4.5]);
+    _xScale.range([0, chartWidth]);
+    return _xScale;
+  }, []);
+
+  const yScale = useMemo(() => {
+    const _yScale = scaleLinear();
+    _yScale.domain([-4.5 * k, 4.5 * k]);
+    _yScale.range([chartHeight, 0]);
+    return _yScale;
+  }, []);
+
+  const [rev, bumpRev] = useRev();
+  const {
+    onPointerDown,
+    onPointerUp,
+    onWheelZoom,
+  } = usePanZoom({
+    xScale,
+    yScale,
+    preserveAspectRatio: true,
+    onUpdate: () => {
+      bumpRev();
+    },
+    registerMoveListener: (onPointerMove) => {
+      // Only listen to move events while an interaction is happening.
+      // Listen on `document` so that a panning gesture can continue beyond the edge of the chart.
+      const handlePointermove = (e: PointerEvent) => {
+        onPointerMove(e.pointerId, {
+          x: e.clientX - chartOffset.current.x,
+          y: e.clientY - chartOffset.current.y,
+        });
+      };
+      document.addEventListener('pointermove', handlePointermove, {passive: false});
+      return () => {
+        document.removeEventListener('pointermove', handlePointermove);
+      };
+    },
+  });
+
+  // preventDefault of certain events.
+  // This can't be done in JSX callbacks because they can't specify `{passive: false}`.
+  useEffect(() => {
+    if (!chartElement) return;
+    const preventDefault = (e: Event) => {
+      e.preventDefault();
+    };
+    // 'touchstart' needs to be canceled or else the UA will sometimes hijack the touch for scrolling, page navigation ("swipe"), or full-page pixelated zooming.
+    chartElement.addEventListener('touchstart', preventDefault, {passive: false});
+    // 'wheel' needs to be canceled so that the page doesn't scroll.
+    chartElement.addEventListener('wheel', preventDefault, {passive: false});
+    return () => {
+      chartElement.removeEventListener('touchstart', preventDefault);
+      chartElement.removeEventListener('wheel', preventDefault);
+    }
+  }, [chartElement]);
+
+  return (
+    <div>
+      <p>
+        This is a reproduction of the d3 <a href="https://observablehq.com/@d3/zoomable-scatterplot" target="_blank">Zoomable Scatterplot</a> example.
+      </p>
+      <div style={{
+        width: chartWidth,
+        height: chartHeight,
+        border: '1px solid #666',
+        position: 'relative',
+      }}>
+        <div style={{
+          position: 'absolute',
+          inset: 0,
+        }}>
+          <svg
+            ref={setChartElement}
+            width={chartWidth}
+            height={chartHeight}
+            viewBox={`0 0 ${chartWidth} ${chartHeight}`}
+            style={{
+              overflow: 'hidden',
+              userSelect: 'none',
+            }}
+            onPointerDown={(e) => {
+              // Only listen to primary button events (no right-clicks, etc).
+              if (e.button !== 0) return;
+              // Take note of the chart's on-screen position when the gesture starts.
+              updateChartOffset();
+              // Capturing the pointer lets panning gestures avoid being interrupted when they stray outside the window bounds.
+              e.currentTarget.setPointerCapture(e.pointerId);
+              // Report a pointer down, passing coordinates relative to the chart.
+              onPointerDown(e.pointerId, {
+                x: e.clientX - chartOffset.current.x,
+                y: e.clientY - chartOffset.current.y,
+              });
+            }}
+            onPointerUp={(e) => {
+              e.currentTarget.releasePointerCapture(e.pointerId);
+              onPointerUp(e.pointerId);
+            }}
+            onPointerLeave={(e) => {
+              onPointerUp(e.pointerId);
+            }}
+            onPointerCancel={(e) => {
+              onPointerUp(e.pointerId);
+            }}
+            onWheel={(e) => {
+              // Take note of the chart's on-screen position.
+              updateChartOffset();
+              // Report a wheel zoom event, passing coordinates relative to the chart.
+              onWheelZoom({
+                center: {
+                  x: e.clientX - chartOffset.current.x,
+                  y: e.clientY - chartOffset.current.y,
+                },
+                zoomRatio: Math.pow(2, normalizeWheelDelta({
+                  delta: e.deltaY,
+                  deltaMode: e.deltaMode,
+                  multiplier: e.ctrlKey ? 10 : 1,
+                })),
+              });
+            }}
+          >
+            <g fill="none" stroke-linecap="round" stroke-width="1.1000555909229373">
+              {data.map((d, i) => (
+                <path key={i}
+                  d={`M${xScale(d[0])},${yScale(d[1])}h0`}
+                  stroke={colorScale(d[2] as unknown as string)}
+                  strokeWidth={5}
+                />
+              ))}
+            </g>
+            <g transform={`translate(${chartWidth}, 0)`}>
+              <Axis
+                orient="left"
+                scale={yScale}
+                tickArguments={[12]}
+                color='#1b1e23'
+                strokeColor='rgba(0, 0, 0, 0.1)'
+                tickSizeInner={chartWidth - 40}
+                tickSizeOuter={chartWidth - 40}
+                scaleRev={rev}
+              />
+            </g>
+            <g transform={'translate(0, 0)'}>
+              <Axis
+                orient="bottom"
+                scale={xScale}
+                tickArguments={[12 * k]}
+                color='#1b1e23'
+                strokeColor='rgba(0, 0, 0, 0.1)'
+                tickSizeInner={chartHeight - 20}
+                tickSizeOuter={chartHeight - 20}
+                scaleRev={rev}
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
